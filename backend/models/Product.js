6d835e1dbf7982ac0c2cb531f4192ce4bbf52ce0
fk-crafter <fowad.kh@outlook.fr>
2025-02-09 02:17:30 +0100

    tst

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -33,6 +33,6 @@ const productSchema = new mongoose.Schema(
   }
 );
 
-const Product = mongoose.model("Product", productSchema);
+const Produc = mongoose.model("Product", productSchema);
 
 module.exports = Product;
