f25608e2d7544d724488ea2e643d7d6f39680255
fk-crafter <fowad.kh@outlook.fr>
2025-02-22 14:42:42 +0100

    update: social added on footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import Nintendologo from "@/public/img/nintendologo.png";
+import Link from "next/link";
+import { Facebook, Instagram } from "lucide-react";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
   return (
@@ -15,12 +18,12 @@ export default function Footer() {
           <p className="text-sm mt-2">Lorem ipsum dolor sit amet</p>
         </div>
         <div className="flex flex-col space-y-2">
-          <a href="#" className="hover:underline">
+          <Link href="#" className="hover:underline">
             About Us
-          </a>
-          <a href="#" className="hover:underline">
+          </Link>
+          <Link href="#" className="hover:underline">
             Terms of Service
-          </a>
+          </Link>
           <a href="#" className="hover:underline">
             Privacy Policy
           </a>
@@ -31,15 +34,23 @@ export default function Footer() {
           <p>Phone: +123 456 789</p>
         </div>
         <div className="flex space-x-4 mt-4 md:mt-0">
-          <a href="#" aria-label="Facebook" className="hover:opacity-75">
-            {/* Icône Facebook */}
-          </a>
-          <a href="#" aria-label="Twitter" className="hover:opacity-75">
-            {/* Icône Twitter */}
-          </a>
-          <a href="#" aria-label="Instagram" className="hover:opacity-75">
-            {/* Icône Instagram */}
-          </a>
+          <Link
+            href="https://facebook.com"
+            target="_blank"
+            aria-label="Facebook"
+          >
+            <Facebook className="w-6 h-6 hover:opacity-75 transition" />
+          </Link>
+          <Link href="https://x.com" target="_blank" aria-label="X (Twitter)">
+            <FaXTwitter className="w-6 h-6 hover:opacity-75 transition" />
+          </Link>
+          <Link
+            href="https://instagram.com"
+            target="_blank"
+            aria-label="Instagram"
+          >
+            <Instagram className="w-6 h-6 hover:opacity-75 transition" />
+          </Link>
         </div>
       </div>
     </footer>
