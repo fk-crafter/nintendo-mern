250bfa7c03cdc96522c240725910729b6db54291
fk-crafter <fowad.kh@outlook.fr>
2025-03-03 12:27:50 +0100

    image

--- backend/dist/routes/uploadRoutes.js
+++ backend/dist/routes/uploadRoutes.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const multer_1 = __importDefault(require("multer"));
@@ -11,25 +13,25 @@ const router = (0, express_1.Router)();
 // Assure-toi que le dossier "uploads" existe
 const uploadDir = path_1.default.join(__dirname, "../uploads");
 if (!fs_1.default.existsSync(uploadDir)) {
-    fs_1.default.mkdirSync(uploadDir, { recursive: true });
+  fs_1.default.mkdirSync(uploadDir, { recursive: true });
 }
 // Configuration de multer
 const storage = multer_1.default.diskStorage({
-    destination: (_req, _file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (_req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
+  destination: (_req, _file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (_req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 const upload = (0, multer_1.default)({ storage });
 // Route POST pour uploader une image
 router.post("/", upload.single("image"), (req, res, next) => {
-    if (!req.file) {
-        res.status(400).json({ message: "Aucun fichier n'a été téléchargé." });
-        return;
-    }
-    const imageUrl = `http://localhost:5001/uploads/${req.file.filename}`;
-    res.status(201).json({ imageUrl });
+  if (!req.file) {
+    res.status(400).json({ message: "Aucun fichier n'a été téléchargé." });
+    return;
+  }
+  const imageUrl = `${process.env.BACKEND_URL}/uploads/${req.file.filename}`;
+  res.status(201).json({ imageUrl });
 });
 exports.default = router;
