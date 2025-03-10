eeb037bb72813ce6e64bfd26c1574a0b5aba3cec
fk-crafter <fowad.kh@outlook.fr>
2025-03-10 17:34:24 +0100

    refactor backend code

--- backend/server.ts
+++ backend/server.ts
@@ -2,11 +2,13 @@ import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db";
+
 import userRoutes from "./routes/userRoutes";
 import productRoutes from "./routes/productRoutes";
 import orderRoutes from "./routes/orderRoutes";
 import authRoutes from "./routes/authRoutes";
 import statsRoutes from "./routes/statsRoutes";
+
 import uploadRoutes from "./routes/uploadRoutes";
 import path from "path";
 
@@ -15,17 +17,8 @@ connectDB();
 
 const app = express();
 
-// Configurer les en-têtes CORS dynamiquement avec des variables d'environnement
-const allowedOrigins = [
-  "http://localhost:3000", // Environnement local
-  process.env.FRONTEND_URL, // URL de ton frontend en production (définie dans Vercel)
-];
-
 app.use((req: Request, res: Response, next: NextFunction) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin as string)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
@@ -33,19 +26,18 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 
 app.use(express.json());
 
-// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/stats", statsRoutes);
+
 app.use("/api/upload", uploadRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// CORS avec option dynamique
 app.use(
   cors({
-    origin: allowedOrigins, // Liste des origines autorisées
+    origin: "http://localhost:3000",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   })
