96396e0a1876d8d6542bfe5120fa8751731a185c
fk-crafter <fowad.kh@outlook.fr>
2025-03-10 15:22:36 +0100

    Fix MongoDB connection and TypeScript conversion

--- backend/dist/config/db.js
+++ backend/dist/config/db.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,10 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const mongoose = require("mongoose");
-const connectDB = () => __awaiter(this, void 0, void 0, function* () {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const conn = yield mongoose.connect(process.env.MONGO_URI);
+        const conn = yield mongoose_1.default.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log(`✅ MongoDB connectée : ${conn.connection.host}`);
     }
     catch (error) {
@@ -18,4 +26,4 @@ const connectDB = () => __awaiter(this, void 0, void 0, function* () {
         process.exit(1);
     }
 });
-module.exports = connectDB;
+exports.default = connectDB;
