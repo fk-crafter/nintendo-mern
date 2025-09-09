6894b195b7d1c4ff8fe76e30644f6f0d706962c4
fk-crafter <fowad.kh@outlook.fr>
2025-09-09 08:23:16 +0200

    remove useeffect in login

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
